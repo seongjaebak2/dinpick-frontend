f15b1c652a3722cb9d1c54104830aa700cf96c19
GBK <lfejku17@gmail.com>
2026-01-02 11:30:36 +0900

    feat: 부트스트랩 라이브러리 추가 및 적용

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import { HashRouter } from "react-router-dom";
 import App from "./App";
 import "react-toastify/dist/ReactToastify.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
