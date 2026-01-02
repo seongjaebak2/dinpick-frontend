f9f0be3db137049052565fa0e19f222e05ad878d
GBK <lfejku17@gmail.com>
2026-01-02 15:45:36 +0900

    style: 라우팅 주석 추가 및 api 파일 이름 수정

--- src/App.jsx
+++ src/App.jsx
@@ -14,9 +14,13 @@ const App = () => {
   return (
     <AuthProvider>
       <Routes>
+        {/* 홈페이지 */}
         <Route path="/" element={<HomePage />} />
+        {/* 레스토랑 페이지 */}
         <Route path="/restaurants" element={<RestaurantsPage />} />
+        {/* 레스토랑 상세 페이지 */}
         <Route path="/restaurants/:id" element={<RestaurantDetailPage />} />
+        {/* 마이페이지(인증된 유저만 접근 가능) */}
         <Route
           path="/me"
           element={
@@ -25,6 +29,7 @@ const App = () => {
             </ProtectedRoute>
           }
         />
+        {/* 로그인 & 회원가입 페이지 (인증된 유저는 접근 불가) */}
         <Route
           path="/login"
           element={
@@ -41,8 +46,10 @@ const App = () => {
             </AuthRedirect>
           }
         />
+        {/* 존재하지 않는 경로로 접근 시 홈으로 이동 */}
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
+      {/* 3초 유지 토스트박스 */}
       <ToastContainer position="top-center" autoClose={3000} theme="light" />
     </AuthProvider>
   );
